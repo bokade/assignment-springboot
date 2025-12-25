21f208d7324015c98b0df36e2976249371d31607
Swapnil Bokade <sbokade181@gmail.com>
2025-12-25 11:25:54 +0530

    changes

--- src/main/java/com/example/utils/i11Utils.java
+++ src/main/java/com/example/utils/i11Utils.java
@@ -12,4 +12,6 @@ public class i11Utils {
     public static final String MOBILE_PATTERN = "^[6-9][0-9]{9}$";
 
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static final String EMAIL_PATTERN_PROD = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 }
